d37d0faf7c9969d49cef7863c2fbc8afef523d6d
ShirotaSora <oibc35293@ojs.ac.jp>
2024-04-18 14:48:07 +0900

    静的クラスに変更

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -13,18 +13,16 @@ namespace DistanceConverter {
 
         //フィートからメートルへの対応表を出力
         static void PrintFeetToMeterList(int start, int stop) {
-            FeetConverter fc = new FeetConverter();
             for (int feet = start; feet <= stop; feet++) {
-                double meter = fc.ToMeter(feet);
+                double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine("{0}ft = {1:0.0000} m", feet, meter);
             }
         }
 
         //メートルからフィートへの対応表を出力
         static void PrintMeterToFeetList(int start, int stop) {
-            FeetConverter fc= new FeetConverter();
             for (int meter = start; meter <= stop; meter++) {
-                double feet = fc.FromMeter(meter);
+                double feet = FeetConverter.FromMeter(meter);
                 Console.WriteLine("{0}m = {1:0.0000} ft", meter, feet);
             }
         }
