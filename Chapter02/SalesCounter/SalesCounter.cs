22984ff2eb4f55f089193ba2f7c855d0e84fca25
ShirotaSora <oibc35293@ojs.ac.jp>
2024-04-24 14:49:33 +0900

    インターフェース

--- Chapter02/SalesCounter/SalesCounter.cs
+++ Chapter02/SalesCounter/SalesCounter.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace SalesCounter {
     public class SalesCounter {
-        private List<Sale> _sales;
+        private IEnumerable<Sale> _sales;
 
         //コンストラクタ
         public SalesCounter(string filePass) {
@@ -16,7 +16,7 @@ namespace SalesCounter {
         }
 
         //売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        private static List<Sale> ReadSales(string filePath) {
+        private static IEnumerable<Sale> ReadSales(string filePath) {
             List<Sale> sales = new List<Sale>();
             string[] lines = File.ReadAllLines(filePath);
             foreach (string line in lines) {
@@ -32,7 +32,7 @@ namespace SalesCounter {
         }
 
         //店舗別の売り上げを求める
-        public Dictionary<string, int> GetPerStoreSales() {
+        public IDictionary<string, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (Sale sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName)) {
