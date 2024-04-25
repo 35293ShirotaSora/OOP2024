b6ba57644d0a251a50c688d2178176513bc8e982
ShirotaSora <oibc35293@ojs.ac.jp>
2024-04-25 14:47:56 +0900

    chapter02 終了

---
+++ Exercise2/YardConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    internal class YardConverter {
+        public static readonly double ratio = 0.9144;
+
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+        public static double ToMeter(double yard) {
+            return yard * ratio;
+        }
+    }
+}
