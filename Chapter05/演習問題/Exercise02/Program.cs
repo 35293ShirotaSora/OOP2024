efbdc985b1d7e03342080a7e63f45997f97878cb
ShirotaSora <oibc35293@ojs.ac.jp>
2024-06-06 13:41:00 +0900

    7完成

--- Chapter05/演習問題/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,10 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            Console.Write("数列1:");
+            Console.Write("数列:");
             var s1 = Console.ReadLine();
-            Console.Write("数列2:");
-            var s2 = Console.ReadLine();
-            
+            var n1 = int.Parse(s1);
+            Console.WriteLine(int.TryParse(n1));
         }
     }
 }
