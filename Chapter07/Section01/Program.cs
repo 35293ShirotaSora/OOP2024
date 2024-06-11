cd6520cb8bb2fce253a52139258ff3ee44f58f3b
ShirotaSora <oibc35293@ojs.ac.jp>
2024-06-11 15:52:30 +0900

    １期end

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -7,15 +7,20 @@ using System.Threading.Tasks;
 
 namespace Section01 {
     internal class Program {
+        static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
+
         static void Main(string[] args) {
-            var prefOfficeDict = new Dictionary<string, string>();
-            string pref;
-            string prefcaploc;
+            string pref, prefcaploc;
             Console.WriteLine("県庁所在地の登録");
 
-            for(int i = 0; i < 5; i++) {
+            while (true) {
                 Console.Write("都道府県:");
                 pref = Console.ReadLine();
+
+                if(pref == null) {
+                    break;
+                }
+
                 Console.Write("県庁所在地:");
                 prefcaploc = Console.ReadLine();
                 if (prefOfficeDict.ContainsKey(pref)) {
@@ -30,46 +35,50 @@ namespace Section01 {
             Console.WriteLine();
 
             Boolean endFlag = false;
-            while (true) {
-                Console.WriteLine("**** メニュー ****");
-                Console.WriteLine("１: 一覧表示");
-                Console.WriteLine("２: 検索");
-                Console.WriteLine("９: 終了");
-                Console.Write("＞");
-                string menuSelect = Console.ReadLine();
-                Console.WriteLine();
-                switch (menuSelect) {
+            while (!endFlag) {
+                switch (menuDisp()) {
                     case "1":
-                        foreach (var items in prefOfficeDict) {
-                            Console.WriteLine($"{items.Key}の県庁所在地は{items.Value}です");
-                        }
-                        Console.WriteLine();
-                        break;
-                    
-                    case"2":
-                        Console.WriteLine("都道府県");
-                        string searchPref = Console.ReadLine();
-                        Console.WriteLine($"{searchPref}の県庁所在地は{prefOfficeDict[searchPref]}です。");
+                        allDisp();
                         Console.WriteLine();
                         break;
 
-                    case"9":
+                    case "2":
+                        searchMethod();
+                        break;
+
+                    case "9":
                         endFlag = true;
                         break;
                 }
-                if (endFlag) {
-                    break;
-                }
             }
+        }
 
-            
+      
+        private static void allDisp() {
+            foreach (var items in prefOfficeDict) {
+                Console.WriteLine($"{items.Key}の県庁所在地は{items.Value}です");
+            }
+        }
 
+        private static void searchMethod() {
+            Console.WriteLine("都道府県");
+            string searchPref = Console.ReadLine();
+            Console.WriteLine($"{searchPref}の県庁所在地は{prefOfficeDict[searchPref]}です。");
+            Console.WriteLine();
+        }
 
+        private static string menuDisp() {
+            Console.WriteLine("**** メニュー ****");
+            Console.WriteLine("１: 一覧表示");
+            Console.WriteLine("２: 検索");
+            Console.WriteLine("９: 終了");
+            Console.Write("＞");
+            string menuSelect = Console.ReadLine();
+            Console.WriteLine();
+            return menuSelect;
+        }
 
-
-
-
-           /* var employeeDict = new Dictionary<int, Employee> {
+        /* var employeeDict = new Dictionary<int, Employee> {
                { 100, new Employee(100, "清水遼久") },
                { 112, new Employee(112, "芹沢洋和") },
                { 125, new Employee(125, "岩瀬奈央子") },
@@ -80,7 +89,5 @@ namespace Section01 {
             foreach( var item in employeeDict) {
                 Console.WriteLine($"{item}");
             }*/
-
-        }
     }
 }
