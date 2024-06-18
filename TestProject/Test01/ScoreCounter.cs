b88ff422fc886a4b8490bd29f2d57821bd9949c1
ShirotaSora <oibc35293@ojs.ac.jp>
2024-06-18 11:13:56 +0900

    設問１_提出

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -11,22 +11,33 @@ namespace Test01 {
         }
 
 
-        //メソッドの概要： 
+        //メソッドの概要：スコアデータを読み込み、Studentオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
-
-
-
-
-
+            List<Student> scores = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                string[] item = line.Split(',');
+                Student score = new Student {
+                    Name = item[0],
+                    Subject = item[1],
+                    Score = int.Parse(item[2]),
+                };
+                scores.Add(score);
+            }
+            return scores;
         }
 
-        //メソッドの概要： 
+        //メソッドの概要：学生別の点数を求める
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _score) {
+                if (dict.ContainsKey(sale.Name)) {
+                    dict[sale.Name] += sale.Score;
+                } else {
+                    dict[sale.Name] = sale.Score;
+                }
+            }
+            return dict;
         }
     }
 }
