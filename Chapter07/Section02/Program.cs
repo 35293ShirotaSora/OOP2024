8551ed3e0aafb272851fe64340c53830c657a712
ShirotaSora <oibc35293@ojs.ac.jp>
2024-06-18 10:37:24 +0900

    chapter07 Section02

---
+++ Chapter07/Section02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            /*var dict = new Dictionary<MonthDay, string> {
+                    { new MonthDay(3, 5), "珊瑚の日" },
+                    { new MonthDay(8, 4), "箸の日" },
+                    { new MonthDay(10, 3), "登山の日" },
+            };
+            var md = new MonthDay(8, 4);
+            var s = dict[md];
+            Console.WriteLine(s);*/
+
+            var lines = File.ReadAllLines("sample.txt");
+            var we = new WordsExtractor(lines);
+            foreach (var word in we.Extract()) {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
