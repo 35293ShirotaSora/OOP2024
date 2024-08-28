b9a32149cbe554f0825dc36e81e77ff3f5741527
ShirotaSora <oibc35293@ojs.ac.jp>
2024-08-28 15:49:25 +0900

    演習問題1終了

--- Chapter12/演習問題/Exercise01/Employee.cs
+++ Chapter12/演習問題/Exercise01/Employee.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Security.Policy;
-
-public class Employee {
-    public int Id { get; set; }
-
-    public string Name { get; set; }
-
-    public DateTime HireDate { get; set; }
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+namespace Exercise01 {
+    public class Employee {
+        [JsonIgnore]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("hireDate")]
+        public DateTime HireDate { get; set; }
+    }
 }
+
--- Chapter12/演習問題/Exercise01/Program.cs
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
@@ -24,10 +28,10 @@ namespace Exercise01 {
             Console.WriteLine(File.ReadAllText("employees.xml"));
             Console.WriteLine();
 
-            //Exercise1_4("employees.json");
+            Exercise1_4("employees.json");
 
             // これは確認用
-            //Console.WriteLine(File.ReadAllText("employees.json"));
+            Console.WriteLine(File.ReadAllText("employees.json"));
         }
 
         private static void Exercise1_1(string outfile) {
@@ -87,8 +91,32 @@ namespace Exercise01 {
             }
         }
 
-        private static void Exercise1_4(string v) {
-        
+        private static void Exercise1_4(string file) {
+            var emps = new Employee[] {
+                new Employee {
+                    Id = 123,
+                    Name = "佐藤",
+                    HireDate = new DateTime(2012, 5, 3)
+                },
+
+                new Employee {
+                    Id = 210,
+                    Name = "斎藤",
+                    HireDate = new DateTime(2009, 1, 23)
+                },
+
+            };
+
+            using (var stream = new FileStream(file, FileMode.Create, FileAccess.Write)) { 
+
+                var options = new JsonSerializerOptions {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                    WriteIndented = true,
+                };
+
+              JsonSerializer.Serialize(stream,emps, options);
+                
+            }
         }
     }
 }
