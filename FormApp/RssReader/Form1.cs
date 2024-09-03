a2d545f6733e69d345c29abe227eb31829d50cb1
ShirotaSora <oibc35293@ojs.ac.jp>
2024-09-03 11:36:02 +0900

    タイトル表示

---
+++ FormApp/RssReader/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbRssUrl.Text);
+                var xdoc = XDocument.Load(url);
+                var xtitles = xdoc.Root.Descendants("item")
+                                       .Elements("title");
+                foreach (var xtitle in xtitles) {
+                    lbRssTitle.Items.Add(xtitle.Value);
+                }
+
+                //webBrowser1.Navigate(tbRssUrl.Text);
+
+            }
+        }
+    }
+}
