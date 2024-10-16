c55bd73fdbaa9cc0c51ca8df6abe5a42094c7c30
ShirotaSora <oibc35293@ojs.ac.jp>
2024-10-16 14:02:45 +0900

    17-1プログラムは完成

--- Chapter17/TemplateMethod/TextNumberSizeChange/ToHankakuProcessor.cs
+++ Chapter17/TemplateMethod/TextNumberSizeChange/ToHankakuProcessor.cs
@@ -9,6 +9,10 @@ using TextFileProcessor;
 
 namespace TextNumberSizeChange {
     class ToHankakuProcessor : TextProcessor{
+        Dictionary<char, char> ToHankaku = new Dictionary<char, char>() {
+            {'０','0'},{'１','1'},{'２','2'},{'３','3'},{'４','4'},
+            {'５','5'},{'６','6'},{'７','7'},{'８','8'},{'９','9'}
+        };
 
         private int _count;
 
@@ -18,6 +22,16 @@ namespace TextNumberSizeChange {
 
         protected override void Execute(string line) {
             _count++;
+            string convertedLine = ConvertToHankaku(line);
+            Console.WriteLine(convertedLine);
+        }
+
+        private string ConvertToHankaku(string input) {
+            char[] output = new char[input.Length];
+            for (int i = 0; i < input.Length; i++) {
+                output[i] = ToHankaku.ContainsKey(input[i]) ? ToHankaku[input[i]] : input[i];
+            }
+            return new string(output);
         }
 
         protected override void Terminate() {
