2969ac1c807db670f2d2765951281f84d3d4526c
ShirotaSora <oibc35293@ojs.ac.jp>
2024-10-23 14:58:02 +0900

    stockまで完了

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -18,8 +18,39 @@ namespace ColorChecker {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        private List<MyColor> colorList = new List<MyColor>();
+
+        double r;
+        double g;
+        double b;
+
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            r = (int)rSlider.Value;
+            rValue.Text = r.ToString();
+            g = (int)gSlider.Value;
+            gValue.Text = g.ToString();
+            b = (int)bSlider.Value;
+            bValue.Text = b.ToString();
+
+            var newColor = Color.FromRgb((byte)r, (byte)g, (byte)b);
+
+            colorArea.Background = new SolidColorBrush(newColor);
+        }
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+            r = rSlider.Value;
+            g = gSlider.Value;
+            b = bSlider.Value;
+
+            var color = new MyColor();
+
+            color.Color = Color.FromRgb((byte)r, (byte)g, (byte) b);
+
+            stockList.Items.Add(color.ToString());
         }
     }
 }
---
+++ WPF/ColorChecker/MyColor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ColorChecker {
+    public class MyColor {
+        public Color Color { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public override string ToString() {
+            return $"RGB({Color.R}, {Color.G}, {Color.B})";
+        }
+    }
+}
