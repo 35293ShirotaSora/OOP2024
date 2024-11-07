e113315d07f122d20b84b4f1e392c286b9a2ce28
ShirotaSora <oibc35293@ojs.ac.jp>
2024-11-07 10:51:53 +0900

    最低限完成

---
+++ WPF/SampleWeightUnitConverter/Model/GramUnit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWeightUnitConverter{
+    public class GramUnit : WeightUnit {
+        private static List<GramUnit> units = new List<GramUnit> {
+            new GramUnit{ Name = "g",Coefficient = 1,},
+            new GramUnit{ Name = "kg",Coefficient = 10*100},
+        };
+        public static ICollection<GramUnit> Units { get { return units; } }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="unit"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+
+        public double FromPoundUnit(PoundUnit unit, double value) {
+            return (value * unit.Coefficient) * 28.35 / this.Coefficient;
+        }
+    }
+}
---
+++ WPF/SampleWeightUnitConverter/Model/PoundUnit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWeightUnitConverter{
+    public class PoundUnit : WeightUnit {
+        private static List<PoundUnit> units = new List<PoundUnit> {
+            new PoundUnit{ Name = "lb",Coefficient = 1,},
+            new PoundUnit{ Name = "oz",Coefficient = 16},
+        };
+        public static ICollection<PoundUnit> Units { get { return units; } }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="unit"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+
+        public double FromGramUnit(GramUnit unit, double value) {
+            return (value * unit.Coefficient) / 28.35 / this.Coefficient;
+        }
+    }
+}
