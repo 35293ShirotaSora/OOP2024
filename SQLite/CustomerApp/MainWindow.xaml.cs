1b70236589edb45c10f122ee78dc4b7898c1b94e
ShirotaSora <oibc35293@ojs.ac.jp>
2024-11-14 12:09:46 +0900

    起動時読み込み処理実装

--- SQLite/CustomerApp/MainWindow.xaml.cs
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace CustomerApp {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        List<Customer> _customers;
         public MainWindow() {
             InitializeComponent();
         }
@@ -35,16 +36,43 @@ namespace CustomerApp {
                 connection.CreateTable<Customer>();
                 connection.Insert(customer);
             }
+            ReadDatabase();
         }
 
-        private void ReadButton_Click(object sender, RoutedEventArgs e) { 
+        private void ReadButton_Click(object sender, RoutedEventArgs e) {
+            //ReadDatabase();
+        }
+      
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            var filterList = _customers.Where(x=>x.Name.Contains(SearchTextBox.Text)).ToList();
+            CustomerListView.ItemsSource = filterList;
+        }
 
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+            var item = CustomerListView.SelectedItem as Customer;
+            if (item == null) {
+                MessageBox.Show("削除する行を選択してください");
+                return;
+            }
+            using(var connection = new SQLiteConnection(App.databasePass)) {
+                connection.CreateTable<Customer>();
+                connection.Delete(item);
+
+                ReadDatabase();
+            }
+        }
+
+        private void ReadDatabase() {
             using (var connection = new SQLiteConnection(App.databasePass)) {
                 connection.CreateTable<Customer>();
-                var customers = connection.Table<Customer>().ToList();
+                _customers = connection.Table<Customer>().ToList();
 
-                CustomerListView.ItemsSource = customers;
+                CustomerListView.ItemsSource = _customers;
             }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            ReadDatabase();
         }
     }
 }
